a023f9862d71aa3733211b45013eafdb2a12b38e
Han Yi <b180003@e.ntu.edu.sg>
2020-10-27 21:20:56 +0800

    Added Filter Variables

--- backend/mazerunner/api/urls.py
+++ backend/mazerunner/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import StudentAPIView , QuestionAPIView ,CreateQuestionAPIView , gameSummaryAPIView  , LoginAPIView , LeaderBoardAPIView, overallSummaryView, selectWorld ,QuestionSubmitAPIView
+from .views import StudentAPIView , QuestionAPIView ,CreateQuestionAPIView , gameSummaryAPIView  , LoginAPIView , LeaderBoardAPIView, overallSummaryView, QuestionSubmitAPIView
 
 urlpatterns = [
     path('api/login/',LoginAPIView.as_view(), name = 'login'),
@@ -11,6 +11,5 @@ urlpatterns = [
     path('api/questions/create', CreateQuestionAPIView.as_view() , name = 'create-questions'),
     path('api/gameSummary', gameSummaryAPIView.as_view() , name = 'gameSummary'),
 
-    path('dashboard/', overallSummaryView.as_view(), name='dashboard'),
-    path('<str:worldID>/world/', selectWorld, name='select_world')
+    path('dashboard/', overallSummaryView.as_view(), name='dashboard')
 ]
